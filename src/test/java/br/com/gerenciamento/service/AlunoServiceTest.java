2a2e1a90c461d321609b57f2cafa4f641d480294
JorgRibeiro <jorge.ribeiro@ufape.edu.br>
2025-06-18 11:11:11 -0300

    Implementacao de 4 testes unitarios em AlunoServiceTest
    2
    
    feat: Implementa 4 testes unitários para AlunoService

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertThrows;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
@@ -44,4 +46,77 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    @Test
+    public void statusAtualizado() {
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Ribeiro");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123333");
+        this.serviceAluno.save(aluno);
+
+        Long AlunoID = aluno.getId();       // O id é gerado automativamente pelo servoce, então não precisamos setar apenas pegar o valor gerado
+
+        Aluno alunoRetorno = this.serviceAluno.getById(AlunoID);
+        Assert.assertTrue(alunoRetorno.getStatus().equals(Status.ATIVO));
+
+        aluno.setStatus(Status.INATIVO);
+        this.serviceAluno.save(aluno);
+
+        alunoRetorno = this.serviceAluno.getById(AlunoID);
+        Assert.assertTrue(alunoRetorno.getStatus().equals(Status.INATIVO));
+    }
+
+    @Test
+    public void deletarAluno() {
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Mateus");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("111222");
+        this.serviceAluno.save(aluno);
+
+        Long AlunoID = aluno.getId();
+
+        Assert.assertEquals(this.serviceAluno.getById(AlunoID).getNome(),aluno.getNome());
+
+        this.serviceAluno.deleteById(AlunoID);
+
+        assertThrows(Exception.class, () -> {
+            this.serviceAluno.getById(AlunoID);
+        });
+
+    }
+
+    @Test
+    public void alunoComNomeGrande() {
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon");
+        // Nome completo de Dom Pedro I
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("111222");
+
+        assertThrows(Exception.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+
+    }
+
+    @Test
+    public void procurarAlunoInexistente() {
+
+        assertThrows(Exception.class, () -> {
+            this.serviceAluno.getById(312L);
+        });
+
+    }
+
 }
